c2c9a168fad99e516e9f6e606f6d5e9e419163b6
Daniel <danielhernandez4352@gmail.com>
2022-02-07 11:11:36 +0100

    MenuController, hello-view.fxml

---
+++ src/main/java/com/example/login/MenuController.java
@@ -0,0 +1,17 @@
+package com.example.login;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class MenuController {
+
+    @FXML
+    private ImageView ingresos;
+
+    @FXML
+    public void ingresosClick(){
+        System.out.printf("adasdsad");
+    }
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module com.example.login {
+ module com.example.login {
     requires javafx.controls;
     requires javafx.fxml;
 
