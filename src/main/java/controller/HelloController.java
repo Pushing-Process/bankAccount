faefe965c2788f222aadc7a7c8d272fe629c3440
Daniel <danielhernandez4352@gmail.com>
2022-02-08 12:01:50 +0100

    menu_view.fxml imagenes.

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,17 +1,11 @@
 package controller;
 
-import com.example.login.HelloApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import model.Cuenta;
 
 import java.io.IOException;
 import java.net.URL;
@@ -21,11 +15,14 @@ public class HelloController implements Initializable {
     @FXML
     public TextField userTextField;
 
+
     @FXML
     public PasswordField passwordTextField;
 
     @FXML
     public Button loginButton;
+
+    static SceneController s = new SceneController();
 
     @FXML
     public void OnLoginButtonClick(ActionEvent event) {
@@ -36,7 +33,6 @@ public class HelloController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         loginButton.setOnMouseClicked(mouseEvent -> {
-            SceneController s = new SceneController();
             try {
                 s.switchSceneMenu(mouseEvent);
             } catch (IOException e) {
--- src/main/java/controller/SceneController.java
+++ src/main/java/controller/SceneController.java
@@ -11,15 +11,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class SceneController {
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
 
     public void switchSceneLogin(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("login_view.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.show();
+        Parent parent = FXMLLoader.load(getClass().getResource("login_view.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(parent));
 
     }
 
