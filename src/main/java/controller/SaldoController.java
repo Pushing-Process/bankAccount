66b422c45c323d01f30b6738b9f9caf34a65ba7a
Daniel <danielhernandez4352@gmail.com>
2022-02-14 13:57:29 +0100

    SaldoController finalizado.

--- src/main/java/controller/SaldoController.java
+++ src/main/java/controller/SaldoController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.TextArea;
 import model.Cuenta;
 import model.Extracto;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,6 +23,7 @@ public class SaldoController implements Initializable {
     @FXML
     private Button volverBtn;
 
+    SceneController s = new SceneController();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -34,6 +37,26 @@ public class SaldoController implements Initializable {
             }
             saldoTextArea.appendText(extracto.getSaldo() + "\n");
         }
+
+        volverBtn.setOnMouseClicked(mouseEvent -> {
+            try {
+                s.switchSceneMenu(mouseEvent);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        imprimirBtn.setOnAction(a -> {
+            try {
+                FileWriter myWriter = new FileWriter("factura.txt");
+                myWriter.write(saldoTextArea.getText());
+                myWriter.close();
+                System.out.println("Impreso");
+            } catch (IOException e) {
+                System.out.println("error");
+                e.printStackTrace();
+            }
+        });
 
     }
 }
