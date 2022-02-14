304ab5423420e0f64730581c80556ab04444801d
Daniel <danielhernandez4352@gmail.com>
2022-02-14 11:32:43 +0100

    SaldoController init

--- src/main/java/controller/SaldoController.java
+++ src/main/java/controller/SaldoController.java
@@ -1,4 +1,39 @@
 package controller;
 
-public class SaldoController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import model.Cuenta;
+import model.Extracto;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SaldoController implements Initializable {
+
+    @FXML
+    private TextArea saldoTextArea;
+
+    @FXML
+    private Button imprimirBtn;
+
+    @FXML
+    private Button volverBtn;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Cuenta cuenta = LoginController.cuentaUser;
+        saldoTextArea.setText("Saldo\t\t\t " + cuenta.getBalance() + "\n");
+        for (Extracto extracto : cuenta.getExtractos()) {
+            if (extracto.isIngreso()) {
+                saldoTextArea.appendText("Ingreso\t\t\t");
+            } else {
+                saldoTextArea.appendText("Extracto\t\t\t");
+            }
+            saldoTextArea.appendText(extracto.getSaldo() + "\n");
+        }
+
+    }
 }
